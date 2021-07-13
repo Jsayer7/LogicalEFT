c3f087a7cddf4e430ff8d3b51a4e214f6da49a03
James Sayer <jsayer7@gmail.com>
2021-07-13 12:06:13 -0400

    Adds some comments describing the functions of the Publisher extension methods.

--- LogicEFT/Utilities/Extensions/Publisher+Ext.swift
+++ LogicEFT/Utilities/Extensions/Publisher+Ext.swift
@@ -32,6 +32,12 @@ public extension Publisher where Output == (data: Data, response: URLResponse) {
 }
 
 public extension Publisher where Output == (data: Data, response: HTTPURLResponse) {
+    // Take the (data: Data, response: HTTPURLResponse) tuple object, and check the HTTP status code. If we get a 200, return the raw API Data.
+    // If we get a 400-499 status code, throw an HTTPError.requestFailed error with the encountered status code.
+    // If we get a 500-599 status code, throw an HTTPError.serverError error with the encountered status code.
+    // If we get any other status code, throw an HTTPError.custom error with a string describing the status code we received.
+    
+    /// Take the (data: Data, response: HTTPURLResponse) tuple, and make sure the server sends back a 200 status code before we pass down the raw data.
     func responseData() -> AnyPublisher<Data, HTTPError> {
         tryMap { (data: Data, response: HTTPURLResponse) in
             switch response.statusCode {
@@ -51,6 +57,9 @@ public extension Publisher where Output == (data: Data, response: HTTPURLRespons
 }
 
 public extension Publisher where Output == Data, Failure == HTTPError {
+    // Take the raw API Data object, and decode it to a specified model item. Item here is a generic, but you see on line 38 in EFTMarketAPI.swift we decode the data as an EFTItem object.
+    
+    /// Decode the raw Data to a specified model object that conforms to the Decodable protocol.
     func decoding<Item, Coder>(type: Item.Type, decoder: Coder) -> AnyPublisher<Item, HTTPError> where Item: Decodable, Coder: TopLevelDecoder, Self.Output == Coder.Input {
         decode(type: type, decoder: decoder)
             .mapError { error in
